93143ad9576a680cc41866ebc6b77809503487ac
Hakan Sonmez <hakan.sn.hs@gmail.com>
2023-09-15 18:31:19 +0300

    Clean up some fixme's
    - Improve tests
    - Rename parser
    - Fix code where improved tests failed

--- domenec/src/main.rs
+++ domenec/src/main.rs
@@ -1,12 +1,9 @@
-mod bencode_nom;
+mod bdecode;
 mod bencode;
-mod bencode_fast;
+mod error;
 
 fn main() {
-    // "d1:ad2:xyd20:abcdefghij0123456789i555eee"
     let inp = "d1:ad2:xyd20:abcdefghij0123456789i555eeee";
-    let decoded = bencode_nom::decode(inp);
-    println!("using nom parser => {:?}", decoded);
-    let decoded = bencode_fast::decode(inp.as_bytes());
+    let decoded = bdecode::decode(inp.as_bytes());
     println!("using manuel parser => {:?}", decoded);
 }
